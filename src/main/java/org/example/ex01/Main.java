f775a031c4ffe69bce39a4627f0e551b7d90c391
Fellipev <fellipe2reis@gmail.com>
2023-12-29 15:56:12 -0300

    Repositorio para exercicios de logica de programacao

---
+++ src/main/java/org/example/ex01/Main.java
@@ -0,0 +1,17 @@
+package org.example.ex01;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu
+        //perímetro.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Declare o tamanho da base do retângulo: ");
+        double base = sc.nextDouble();
+        System.out.println("Declare a altura do retângulo: ");
+        double altura = sc.nextDouble();
+    }
+}
